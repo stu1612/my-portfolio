4a3de2e868602d7b6e2659078352b3562e5d0419
Stu Bolderson <stu.bolderson@aol.com>
2024-07-11 12:44:33 +0200

    prismic setup success - slices and page builder initialised - ready to develop

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Urbanist } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import { PrismicPreview } from "@prismicio/next";
+import { repositoryName } from "@/prismicio";
+
+const urbanist = Urbanist({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +18,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="bg-slate-900 text-slate-100">
+      <body className={urbanist.className}>
+        {children}
+        <PrismicPreview repositoryName={repositoryName} />
+      </body>
     </html>
   );
 }
